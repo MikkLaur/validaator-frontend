639ce7e0df62db2bf32af8193c9cb397710ca316
Mikk Laur <mikklaur5@gmail.com>
2018-01-21 14:27:38 +0200

    Better late than later

---
+++ src/main/java/client/HttpClient.java
@@ -0,0 +1,56 @@
+package client;
+
+import com.squareup.okhttp.*;
+import org.json.JSONObject;;
+
+import java.io.IOException;
+
+public class HttpClient {
+    private static OkHttpClient httpClient = new OkHttpClient();
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private static final String URL = "http://0.0.0.0:4567";
+    private HttpClient() {
+
+    }
+
+    static long registerUser(String name, String personalId, String dateOfBirth) {
+        String message = new JSONObject()
+                .put("name", name)
+                .put("personal_id", personalId)
+                .put("date_of_birth", dateOfBirth)
+                .toString();
+        RequestBody body = RequestBody.create(JSON, message);
+
+        Request request = new Request.Builder()
+                .url(URL.concat("/api/users"))
+                .post(body)
+                .build();
+        try {
+            Response response = httpClient.newCall(request).execute();
+            return Long.parseLong(response.body().string());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    static long registerStop(String stopName) {
+        String message = new JSONObject()
+                .put("name", stopName)
+                .toString();
+        RequestBody body = RequestBody.create(JSON, message);
+
+        Request request = new Request.Builder()
+                .url(URL.concat("/api/stops"))
+                .post(body)
+                .build();
+        try {
+            Response response = httpClient.newCall(request).execute();
+            return Long.parseLong(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/client/ValidatorController.java
@@ -0,0 +1,49 @@
+package client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class ValidatorController {
+
+    /* Kasutaja Loomine */
+    public TextField userName;
+    public TextField personal_id;
+    public TextField date_of_birth;
+    public Button registerUserButton;
+    public TableView userHistoryTable;
+
+    public void registerUserClicked(ActionEvent actionEvent) {
+        if (basicDataValidation(userName) && basicDataValidation(personal_id) && basicDataValidation(date_of_birth)) {
+            HttpClient.registerUser(userName.getText(), personal_id.getText(), date_of_birth.getText());
+        }
+    }
+
+    /* Peatuse Loomine */
+    public TextField stopName;
+    public Button registerStopButton;
+
+    public void registerStopClicked(ActionEvent actionEvent) {
+        if (basicDataValidation(stopName)) {
+            HttpClient.registerStop(stopName.getText());
+        }
+    }
+
+
+    /* ValidatorController */
+    public ComboBox selectedUser;
+    public ComboBox selectedStop;
+
+
+    public void loadTransactions(ActionEvent actionEvent) {
+    }
+
+    private boolean basicDataValidation(TextField text) {
+        return text.getText().length() != 0;
+    }
+
+    @FXML
+    private void initialize() {
+        //populate comboboxes and shit
+    }
+}
